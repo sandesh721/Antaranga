b51de0f591f05b06c67233d4d7199ac20a155716
Sandesh <64647847+sandesh721@users.noreply.github.com>
2024-10-15 00:16:17 +0530

    Update supabase.js

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 
-const supabaseUrl = 'https://uzbcnkeihbforjogyyfn.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6YmNua2VpaGJmb3Jqb2d5eWZuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMjQxNDg5OCwiZXhwIjoyMDM3OTkwODk4fQ.AALnlD-5FJek2s91oL_aw2okJw2a_zFe0vTywP4y1ao';
+const supabaseUrl = '';
+const supabaseKey = '';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
