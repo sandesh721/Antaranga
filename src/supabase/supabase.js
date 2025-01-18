b8a7ea1d5123f16bc32bb3ee616f947ef0764d6d
Sandesh <64647847+sandesh721@users.noreply.github.com>
2025-01-18 16:19:20 +0530

    Update supabase.js

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 
 const supabaseUrl = 'https://yobqctmfmdjhkagenbqd.supabase.co';
-const supabaseKey = ;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
